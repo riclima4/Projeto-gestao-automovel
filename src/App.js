8d6ec3264d698068904a132266a20783c25cdba2
Ricardo Lima <ric.lima1@hotmail.com>
2022-10-27 12:09:27 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react'
-import './app.css'
+import React from "react";
+import "./app.css";
+import Navbar from './components/Navbar/Navbar'
+//import Home from './components/home/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+//import Pokeapi from './pages/pokeapi/Pokeapi'
+//import Joke from './pages/joke/Joke'
 
-export default function App() {
+function App() {
   return (
-    <div>App</div>
-  )
+    <>
+    <Router>
+      <Navbar/>
+      <main className="main">
+        <Routes>
+          {/* <Route exact path="/" element={<Home/>} />
+          <Route path="/pokeapi" element={<Pokeapi/>} />
+          <Route path="/joke" element={<Joke/>} /> */}
+        </Routes>
+      </main>
+      </Router>
+    </>
+  );
 }
+
+export default App;
