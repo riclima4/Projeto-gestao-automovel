8dab212d1195b05c12ca92afd8b752c33bf06d92
Ricardo Lima <ric.lima1@hotmail.com>
2022-10-27 16:01:24 +0100

    a

--- src/components/Account/Account.jsx
+++ src/components/Account/Account.jsx
@@ -11,11 +11,19 @@ export default function Account() {
           <div className="title_info">
               <h1>My Account</h1>
               <button className='uil uil-edit-alt btn_edit'>Edit</button>
-            </div>
+          </div>
           <div className="account_info">
-            
             <div className="user_info">
-
+              <div className="row_input">
+                <label htmlFor='nome'>Nome</label>
+                <input className='inputstyle' type="text" name="nome" id="nome" placeholder="Nome do Utilizador"/>
+              </div>
+              <div className="row_input">
+                <label>Email</label>
+                <input className='inputstyle' type="email" name="email" id="email" />
+              </div>
+              
+              
             </div>
           </div>
         </div>    
