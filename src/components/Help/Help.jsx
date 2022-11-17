7da8d6e0b6ecefb6ddc044c2dceeabb988e99fc2
GoncaloPeixoto <goncalopeixoto03@gmail.com>
2022-11-17 17:24:21 +0000

    .
    .

--- src/components/Help/Help.jsx
+++ src/components/Help/Help.jsx
@@ -3,6 +3,8 @@ import "./help.css";
 
 import thumbnail1 from "../../assets/imgs/Thumbnail-1.jpg";
 import thumbnail2 from "../../assets/imgs/Thumbnail-2.jpg";
+import bgRepair from "../../assets/imgs/BGrepair.jpg";
+import bgMecanic from "../../assets/imgs/BGmecanic.jpg";
 
 export default function Help({ data }) {
   return (
@@ -21,60 +23,70 @@ export default function Help({ data }) {
       {/* INFO BANNER-1*/}
       <section className="infoBannerR">
         <div className="infoBannerTextR">
-          <h1>Dicas e boas praticas a realizar</h1>
-          <p>
-            Caso precise de ajuda fornecemos aqui o fórum de dicas da AutoDoc
-            que contem dicas e boas praticas a realizar para uma maior
-            longevidade do seu veículo
-          </p>
+          <div>
+            <h1>Dicas e boas praticas a realizar</h1>
+            <p>
+              Caso precise de ajuda fornecemos aqui o fórum de dicas da AutoDoc
+              que contem dicas e boas praticas a realizar para uma maior
+              longevidade do seu veículo
+            </p>
+          </div>
+          <div className="btn-move">
+            <a
+              className="link-btn"
+              href="https://www.auto-doc.pt/info/category/dicas"
+            >
+              Visitar
+            </a>
+          </div>
+        </div>
+        <div className="flex-img1">
+          <img src={bgRepair} alt="img1"></img>
           <a
-            className="link-btn"
-            href="https://www.auto-doc.pt/info/category/dicas"
+            className="vidBannerIMG"
+            href="https://youtube.com/playlist?list=PLq2YjDQLtrfL0wTAaA2r_xuvjLibppY9O"
           >
-            Visitar
+            <img img src={thumbnail1}></img>
           </a>
         </div>
-        <div className="infoBannerImgR"></div>
       </section>
       {/* INFO BANNER-1 END*/}
       {/* VID BANNER-1*/}
-      <section className="vidBanner">
-        <a
-          className="vidBannerIMG"
-          href="https://youtube.com/playlist?list=PLq2YjDQLtrfL0wTAaA2r_xuvjLibppY9O"
-        >
-          <img img src={thumbnail1}></img>
-        </a>
-      </section>
+      <section className="vidBanner"></section>
       {/* VID BANNER-1END*/}
       {/* INFO BANNER-2*/}
+
       <section className="infoBannerL">
-        <div className="infoBannerImgL"></div>
+        <div className="flex-img2">
+          <img src={bgMecanic} alt="img1"></img>
+          <a
+            className="vidBannerIMG"
+            href="https://youtube.com/playlist?list=PLq2YjDQLtrfL0wTAaA2r_xuvjLibppY9O"
+          >
+            <img img src={thumbnail2}></img>
+          </a>
+        </div>
+
+        {/* INFO BANNER-1 END*/}
+        {/* VID BANNER-1*/}
+        <section className="vidBanner"></section>
         <div className="infoBannerTextL">
-          <h1>Diagnostico do veículo</h1>
+          <h1>Diagnóstico do veículo</h1>
           <p>
-            Caso pretenda realizar um diagnóstico do seu veículo fornecemos o
-            fórum da AutoDoc com informações de ajuda na realização de um
+            Caso pertenda realizar um diagnóstico do seu veículo fornecemos o
+            fórum da AutoDoc com informaçôes de ajuda na realizaçâo de um
             diagnóstico em casa
           </p>
-          <a
-            className="link-btn"
-            href="https://www.auto-doc.pt/info/category/diagnostico"
-          >
-            Visitar
-          </a>
+          <div className="btn-move">
+            <a
+              className="link-btn"
+              href="https://www.auto-doc.pt/info/category/diagnostico"
+            >
+              Visitar
+            </a>
+          </div>
         </div>
       </section>
-      {/* INFO BANNER-1 END*/}
-      {/* VID BANNER-2*/}
-      <section className="vidBanner">
-        <div className="vidBannerIMG">
-          <a href="https://youtube.com/playlist?list=PLq2YjDQLtrfJB44FH0yq7qKH9hPBiMwoB">
-            <img src={thumbnail2}></img>
-          </a>
-        </div>
-      </section>
-      {/* VID BANNER-2END*/}
     </>
   );
 }
